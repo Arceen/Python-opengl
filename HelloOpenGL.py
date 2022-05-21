e12a2bd5a79586596c58f626210d4eb88d63b83e
Niloy <niloyman01@gmail.com>
2022-05-22 00:45:56 +0600

    Getting Started

---
+++ HelloOpenGL.py
@@ -0,0 +1,59 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+pygame.init()
+
+screen_width=800
+screen_height=600
+
+screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF |  OPENGL)
+pygame.display.set_caption('OpenGL in python')
+
+def init_ortho():
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluOrtho2D(0, 640, 0, 480)
+
+done = False
+init_ortho()
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+            
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    
+    glPointSize(10)
+    glBegin(GL_POINTS)
+    glVertex2i(100, 150)
+    glVertex2i(450, 130)
+    glVertex2i(500, 400)
+    glEnd()
+    glPointSize(6)
+    
+    glBegin(GL_POINTS)
+    glVertex2i(455, 145)
+    glEnd()
+    
+    glPointSize(4)
+    glBegin(GL_POINTS)
+    glVertex2i(200, 190)
+    glVertex2i(180, 140)
+    glVertex2i(480, 370)
+    glEnd()
+    glPointSize(3)
+    glBegin(GL_POINTS)
+    glVertex
+    glVertex2i(135, 200)
+    glVertex2i(280, 110)
+    glVertex2i(360, 130)
+    glEnd()
+    pygame.display.flip()
+    pygame.time.wait(100)
+
+pygame.quit()
